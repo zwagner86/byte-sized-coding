dc75736230fb126a29894e66ab41e693e45a38cb
Zach Wagner <zach.wagner@spothero.com>
2018-10-19 11:47:59 -0500

    add body class via helmet in Layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -6,7 +6,9 @@ import './all.sass'
 
 const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet title="Home | Gatsby + Netlify CMS" />
+    <Helmet title="Home | Gatsby + Netlify CMS">
+      <body className="has-navbar-fixed-top"></body>
+    </Helmet>
     <Navbar />
     <div>{children}</div>
   </div>
