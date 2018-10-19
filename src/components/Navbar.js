f62b7b137581e82f5852be2da85b597673813971
Zach Wagner <zach.wagner@spothero.com>
2018-10-19 11:30:10 -0500

    update navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,24 +4,26 @@ import github from '../img/github-icon.svg'
 import logo from '../img/logo.svg'
 
 const Navbar = () => (
-  <nav className="navbar is-transparent">
-    <div className="container">
-      <div className="navbar-brand">
+  <nav className="navbar is-fixed-top">
+    <div className="Navbar-body">
+      <div className="Navbar-left">
         <Link to="/" className="navbar-item">
           <figure className="image">
             <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
           </figure>
         </Link>
       </div>
-      <div className="navbar-start">
-        <Link className="navbar-item" to="/about">
-          About
-        </Link>
-        <Link className="navbar-item" to="/products">
-          Products
-        </Link>
+      <div className="Navbar-center">
+        <div className="Navbar-links">
+          <Link className="navbar-item" to="/about">
+            About
+          </Link>
+          <Link className="navbar-item" to="/products">
+            Products
+          </Link>
+        </div>
       </div>
-      <div className="navbar-end">
+      <div className="Navbar-right">
         <a
           className="navbar-item"
           href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
